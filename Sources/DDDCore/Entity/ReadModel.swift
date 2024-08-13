58180cb8c8fa02d96e5744379c6085c2ab0549fb
Grady Zhuo <gradyzhuo@gmail.com>
2024-08-13 16:04:49 +0800

    [ADJUST] readmodel add sendable

--- Sources/DDDCore/Entity/ReadModel.swift
+++ Sources/DDDCore/Entity/ReadModel.swift
@@ -1,4 +1,4 @@
-public protocol ReadModel: Projectable, Codable {
+public protocol ReadModel: Projectable, Sendable, Codable {
     associatedtype CreatedEventType: DomainEvent
 
     init?(first createdEvent: CreatedEventType, other events: [any DomainEvent]) throws
