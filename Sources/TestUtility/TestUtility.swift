f87449480681ef943aef24d2f12dd09932bf2fa9
Grady Zhuo <gradyzhuo@gmail.com>
2025-07-24 13:52:10 +0800

    [UPDATE] fix build error.

--- Sources/TestUtility/TestUtility.swift
+++ Sources/TestUtility/TestUtility.swift
@@ -26,8 +26,8 @@ extension KurrentDBClient {
 }
 
 extension EventStoreDBClient {
-    public func clearStreams<T: Projectable>(projectableType: T.Type, id: T.ID, revision: KurrentDB.StreamRevision = .any, errorHandler: ((_ error: Error)->Void)? = nil) async {
-        await self.underlyingClient.clearStreams(projectableType: projectableType, id: id, revision: revision, errorHandler: errorHandler)
+    public func clearStreams<T: Projectable>(projectableType: T.Type, id: T.ID, execpted revision: KurrentDB.StreamRevision = .any, errorHandler: ((_ error: Error)->Void)? = nil) async {
+        await self.underlyingClient.clearStreams(projectableType: projectableType, id: id, execpted: revision, errorHandler: errorHandler)
     }
 }
 
