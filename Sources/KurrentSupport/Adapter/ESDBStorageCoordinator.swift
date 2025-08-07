2f177ea8a5059cf6db81438c4b17a3b1413f7b40
Grady Zhuo <gradyzhuo@gmail.com>
2025-08-07 11:33:17 +0800

    Merge pull request #44 from Mendesky/dev/test-bundle
    Dev/test bundle

--- Sources/KurrentSupport/Adapter/ESDBStorageCoordinator.swift
+++ Sources/KurrentSupport/Adapter/ESDBStorageCoordinator.swift
@@ -5,6 +5,7 @@ import Foundation
 import Logging
 
 public class KurrentStorageCoordinator<ProjectableType: Projectable>: EventStorageCoordinator {
+    let logger = Logger(label: "KurrentStorageCoordinator")
     let eventMapper: any EventTypeMapper
     let client: KurrentDBClient
 
@@ -41,7 +42,7 @@ public class KurrentStorageCoordinator<ProjectableType: Projectable>: EventStora
     }
 
     public func fetchEvents(byId id: ProjectableType.ID) async throws -> (events: [any DomainEvent], latestRevision: UInt64)? {
-        let logger = Logger(label: "KurrentStorageCoordinator")
+        
         let streamName = ProjectableType.getStreamName(id: id)
         do{
             let responses = try await client.readStream(.init(name: streamName)){
@@ -50,10 +51,15 @@ public class KurrentStorageCoordinator<ProjectableType: Projectable>: EventStora
             }
 
             let eventWrappers: [(event: any DomainEvent, revision: UInt64)] = try await responses.reduce(into: .init()) {
-                guard let event = try self.eventMapper.mapping(eventData: $1.event.record) else {
-                    return
+                do{
+                    let recordedEvent = try $1.event.record
+                    guard let event = try self.eventMapper.mapping(eventData: recordedEvent) else {
+                        return
+                    }
+                    $0.append((event: event, revision: recordedEvent.revision))
+                }catch {
+                    logger.warning("skipped event cause error happened. error: \(error)")
                 }
-                try $0.append((event: event, revision: $1.event.record.revision))
             }
             
             guard let latestRevision = eventWrappers.last?.revision else {
---
+++ Sources/TestUtility/TestBundle.swift
@@ -0,0 +1,87 @@
+//
+//  IdGenerator.swift
+//  DDDKit
+//
+//  Created by Grady Zhuo on 2025/8/7.
+//
+
+import Foundation
+import KurrentDB
+
+
+public actor TestBundle {
+    public let client: KurrentDBClient
+    public var streamIdentifiers: [StreamIdentifier] = []
+    public let cleanPhase: CleanPhase
+    
+    fileprivate init(client: KurrentDBClient, cleanPhase: TestBundle.CleanPhase) {
+        self.client = client
+        self.streamIdentifiers = []
+        self.cleanPhase = cleanPhase
+    }
+    
+    public func generateAggregateRootId(for category: String, prefix: String = "testing") async -> String {
+        let id = "\(prefix)\(UUID().uuidString)"
+        let streamIdentifier = StreamIdentifier(name: "\(category)-\(id)")
+        self.streamIdentifiers.append(streamIdentifier)
+        
+        if cleanPhase.contains(.begin) {
+            await self.clearStream(streamIdentifier: streamIdentifier)
+        }
+        return id
+    }
+    
+    fileprivate func clearStream(streamIdentifier: StreamIdentifier) async {
+        _ = try? await self.client.deleteStream(streamIdentifier){ options in
+            options.revision(expected: .any)
+        }
+    }
+    
+    public func clearStreams() async {
+        await withTaskGroup { group in
+            for streamIdentifier in self.streamIdentifiers {
+                group.addTask { [unowned self] in
+                    await self.clearStream(streamIdentifier: streamIdentifier)
+                }
+            }
+        }
+        
+    }
+    
+}
+
+extension TestBundle {
+    public struct CleanPhase: OptionSet{
+        public init(rawValue: UInt8) {
+            self.rawValue = rawValue
+        }
+        
+        public var rawValue: UInt8
+        
+        public typealias RawValue = UInt8
+        
+        public static let begin: CleanPhase = .init(rawValue: 1 << 0)
+        public static let end: CleanPhase   = .init(rawValue: 1 << 1)
+        
+        
+        public static let none: CleanPhase  = []
+        public static let both: CleanPhase  = [.begin, .end]
+
+    }
+}
+
+
+public func withTestBundle(client: KurrentDBClient, cleanPhase: TestBundle.CleanPhase = .both, action: (_ bundle: TestBundle) async throws -> Void ) async throws {
+    let bundle = TestBundle(client: client, cleanPhase: cleanPhase)
+    do{
+        try await action(bundle)
+    }catch{
+        print("Test failed with error: \(error)")
+    }
+    
+    if cleanPhase.contains(.end) {
+        await bundle.clearStreams()
+    }
+}
+
+
